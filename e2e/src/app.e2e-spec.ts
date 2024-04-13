3171a4d5c5bf75e63b074a65db65153a9c8a87ac
lChambos <148850250+lChambos@users.noreply.github.com>
2024-04-13 11:04:47 +0200

    first commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Dogecoin app is running!');
+    expect(page.getTitleText()).toEqual('Doge app is running!');
   });
 
   afterEach(async () => {
